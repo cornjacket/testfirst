c6baa2e97ff890cfa190df9e73b8aa2b20736920
David Taylor <taylor.david.ray@gmail.com>
2014-11-23 13:10:03 -0800

    08_book_titles solutions from git

--- 08_book_titles/book.rb
+++ 08_book_titles/book.rb
@@ -1,24 +1,16 @@
 class Book
-  
-  attr_accessor :title  
-  
-  def initialize
-    @book = ""
+  attr_reader :title
+
+  def title=(new_title)
+    words = new_title.split(" ")
+    words = [words[0].capitalize] + words[1..-1].map do |word|
+                 little_words = %w{a an and the in of}
+                 if little_words.include? word
+                   word
+                 else
+                   word.capitalize
+                 end
+             end
+    @title = words.join(" ")
   end
-
-  def title
-    string = @book.split(" ")
-    if string.length == 1
-      @book.capitalize
-    else 
-      #message is broken into string
-	  str2 = string.map { |i| i.capitalize }
-	  #everything is caps
-	  rest = str2[1..-1].join(" ").gsub("And ","and ").gsub("Over ","over ").gsub("The ","the ")
-	  #rest is a string without the first word with replacement
-      str2[0] + ' ' + rest	
-
-    end
-  end
-
 end
